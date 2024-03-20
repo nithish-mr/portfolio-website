43b9ce58f2e48be4ea88e911c093f46b37703eb1
nithish-mr <163519152+nithish-mr@users.noreply.github.com>
2024-03-20 15:13:00 +0530

    Add files via upload

---
+++ src/data/constant.js
@@ -0,0 +1,159 @@
+export const Bio = {
+  name: "Nithish",
+  roles: ["Front End Developer", "React JS Developer", "Javascript Developer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/nithish-mr",
+  resume:
+    "https://drive.google.com/file/d/1ZAtfDb5JBf5BzNNJJioe44tcoCvwJUAC/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/nithish-m-r-",
+  email:"mailto:nithishn133@gmail.com",
+  phone:"tel:+91 8870067703",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React JS",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      // {
+      //   name: "Redux",
+      //   image:
+      //     "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      // },
+      // {
+      //   name: "jQuery",
+      //   image:
+      //     "https://cdn.iconscout.com/icon/free/png-512/free-jquery-8-1175153.png?f=webp&w=256",
+      // },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      // {
+      //   name: "Netlify",
+      //   image:
+      //     "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      // },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.svgrepo.com/show/354202/postman-icon.svg",
+      },
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://saasc10.com/psiber/image/Gallery/syed%20ammal.jpg",
+    school: "syed ammal arts and science college, Ramanathapuram",
+    date: "June 2020 - June 2023",
+    grade: "6.4CGPA , 64%",
+    desc: "I Completed my Bachelor's degree in information Technology at ssyed ammal arts and science college, Ramanathapuram. I have completed 6 semesters and have a CGPA of 6.4.",
+    degree: "Information Technology - BSC ",
+  },
+  {
+    id: 1, 
+    img: "https://saasc10.com/psiber/image/Gallery/syed%20ammal.jpg",
+    school: "syed ammal Secondary School, Ramanathapuram",
+    date: "Apr 2019 - Apr 2020",
+    grade: "60%",
+    desc: "I completed my class 12 high school education at syed ammal Secondary School, Ramanathapuram, where I studied Pure Science.",
+    degree: "HSC(XII),Computer Science",
+  },
+  {
+    id: 2,
+    // img: "https://scontent.fmaa2-4.fna.fbcdn.net/v/t1.6435-9/41675852_377631672774955_7240781168419799040_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=be3454&_nc_ohc=gzLv3vPLK_0AX9pGuct&_nc_ht=scontent.fmaa2-4.fna&oh=00_AfD9cc7dsLc6qX0jcoRtluulwXjsTmiqMiOUVqakZ2uIGg&oe=65D9F0C4",
+    school: "goverment Higher Secondary School, sathirakudi",
+    date: "Apr 2017 - Apr 2018",
+    grade: "60%",
+    desc: "I completed my class 10 education at goverment Higher Secondary School, sathirakudi.",
+    degree: "SSLC(X)"
+  },
+];
+
+export const projects = [
+  // {
+  //   id: 1,
+  //   title: "Weather App",
+  //   date: "Dec 2023",
+  //   description:
+  //     "Developed weather app using Open Weather API to fetch data, dynamically binding it to create an interactive UI for real-time weather updates.",
+  //   image:
+  //     "https://i.ibb.co/DV8zqy2/weatherapp-image.png",
+  //   tags: [
+  //     "HTML",
+  //     "CSS",
+  //     "Javascript",
+  //     "React JS",
+  //   ],
+  //   github: "https://github.com/veeravelS/Weather--app.git",
+  //   webapp: "https://veeravels.github.io/Weather--app/",
+  // },
+  {
+    id: 2,
+    title: "Personal Portfolio Website",
+    date: "Jan 2024",
+    description:
+      "Developed a Dynamic personal portfolio website to showcase skills,project and professional background ,providing comprehensive overview of expertise in web development .",
+    image:
+    "https://i.ibb.co/rdMqy4L/nithish.png" ,
+    tags: ["HTML", "CSS", "Javascript", "Bootstrap", "React JS","Styled Components"],
+    github: "https://github.com/veeravelS/Personal-portfolio-Website.git",
+    webapp: " https://veeravels.github.io/Personal-portfolio-Website/",
+  },
+  // {
+  //   id: 3,
+  //   title: "Form Validation using jQuery",
+  //   date: "Jan 2024",
+  //   description:
+  //     "This dynamic project enhances user experience by implementing robust client-side validation, ensuring accurate and error-free data submissions. It reflects my skills in front-end development.",
+  //   image:
+  //     "https://i.ibb.co/8Xc13XC/Form-validation-image.png",
+  //   tags: ["HTML", "CSS", "Javascript", "Bootstrap", "jQuery"],
+  //   github: "https://github.com/veeravelS/Form-validation.git",
+  //   webapp: "https://veeravels.github.io/Form-validation/",
+  // }
+];
